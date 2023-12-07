d4c8e7f017fbb3fa9a89fc0b27f1080701d792ba
Na Yeon <smilenayeon@naver.com>
2023-12-07 15:42:26 +0900

    formhandler.php practice

---
+++ formhandler.php
@@ -0,0 +1,23 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+   $firstname = htmlspecialchars($_POST["firstname"]);
+   $lastname = htmlspecialchars($_POST["lastname"]);
+   $pet = htmlspecialchars($_POST["favouritepet"]);
+
+   if(empty($firstname | $lastname) ){
+    header("Location:index.php");
+   };
+
+   echo "These are the submited data:";
+   echo "<br>";
+   echo $firstname;
+   echo "<br>";
+   echo $lastname;
+   echo "<br>";
+   echo $pet;
+
+
+
+} else {
+    header("Location:index.php");
+}
